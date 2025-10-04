7110ba0f786cb41fadaec62f9ad0df2b8f35903f
archi-Doc <archidoc422@gmail.com>
2025-10-04 11:14:22 +0900

    .

--- SimpleCommandLine/SimpleParserHelper.cs
+++ SimpleCommandLine/SimpleParserHelper.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
-using static System.Net.Mime.MediaTypeNames;
 
 #pragma warning disable SA1124 // Do not use regions
 
